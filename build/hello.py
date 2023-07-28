4d82f1f648b0add4ca9e63a9a5a08b1a698a0eda
finger92 <zhouyi920521@gmail.com>
2023-07-28 02:25:21 -0400

    update hello

--- build/hello.py
+++ build/hello.py
@@ -1,12 +1,5 @@
 import os
-print(os.getenv("DOMAIN"))
-import os
-env_vars = os.environ
-for key, value in env_vars.items():
-    print(f"{key}: {value}")
-for i in range(10):
-    print(i)
 
 
 def handler(event, context):
-    print(event)
+    print(event, context)
